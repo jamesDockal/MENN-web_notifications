5318df8581dc2a70ec2187c22df96964788933da
James Dockal <jamesdockal@example.com>
2022-06-08 21:19:54 -0300

    feat: create api route to create user

---
+++ src/pages/api/names.ts
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { NextApiRequest, NextApiResponse } from "next";
+
+import connect from "../../services/mongodb";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === "POST") {
+    const { name } = req.body;
+    if (!name) {
+      res.status(400).json({
+        message: "Field 'Name' was not provided!",
+      });
+    }
+
+    const { db } = await connect();
+
+    await db.collection("names").insertOne({ name });
+
+    res.status(201).json({ message: "User created!" });
+  }
+  return;
+};
